90b6116a93ab7e22fefc4bc7a369a3ec9ed730ef
Xochil Squaglia <xochilsquaglia@yahoo.com>
2019-02-16 16:02:39 -0800

    pushing up readme

--- objects/vehicle-class.js
+++ objects/vehicle-class.js
@@ -1,6 +1,34 @@
 'use strict';
 
-class Car {
+
+class Vehicle {
+    constructor(name, wheels) {
+        this.name=name;
+        this.wheels=wheels;
+    }
+    drive() {
+        return "Moving Forward";
+    }
+    stop() {
+        return "Stopping";
+    }
+}
+class Car extends Vehicle{
+    constructor(name){
+        super (name, 4);
+    }
+
+
 }
 
-module.exports = Car;
+class Motorcycle extends Vehicle {
+    constructor(name){
+        super(name, 2);
+    }
+    wheelie() {
+        return "Wheee";
+    }
+
+}
+
+module.exports = {Car, Motorcycle};
---
+++ objects/vehicle-factory.js
@@ -0,0 +1,38 @@
+'use strict';
+//---------------------------------------------------------------
+// The traditional constructor is needed for a factory
+
+const Vehicle = function(name, wheels) {
+    this.name = name;
+    this.wheels = wheels;
+    this.drive = () => {
+        return 'Moving Forward';
+    };
+    this.stop = () => {
+        return 'Stopping';
+    };
+};
+
+//----------------------------------------------------------------
+function Car(name) {
+    // This 'should' have the same interface as Vehicle but it should add "color"
+    const newCar = Object.assign(
+        // {},
+        new Vehicle(name, 4) // Creates the inheritance pattern
+    );
+    return Object.freeze(newCar);
+}
+
+function Motorcycle(name) {
+    const newMotorcycle = Object.assign(
+        // {},
+        {wheelie},
+        new Vehicle(name, 2),
+    );
+    function wheelie() {
+        return 'Wheee!'
+    }
+    return Object.freeze(newMotorcycle)
+}
+
+module.exports = {Car, Motorcycle};
